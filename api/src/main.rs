370872ab65167d1afa3a1d07e43ca84380738c7c
tk1351 <takakikitsui0111@gmail.com>
2025-08-14 20:21:24 +0900

    feat: add hello and get_taskts

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,8 +1,18 @@
-use axum::{Router, routing::get};
+use axum::{Router, response::Json, routing::get};
+
+async fn hello() -> &'static str {
+    "Hello world"
+}
+
+async fn get_tasks() -> Json<Vec<String>> {
+    Json(Vec::new())
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello World" }));
+    let app = Router::new()
+        .route("/", get(hello))
+        .route("/tasks", get(get_tasks));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     axum::serve(listener, app).await.unwrap();
