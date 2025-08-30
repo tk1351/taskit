2c25c15eb6423c3b695ed55569a6a860de8d8d36
tk1351 <takakikitsui0111@gmail.com>
2025-08-30 14:20:49 +0900

    feat: logger

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,11 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { logger } from "hono/logger";
 import user from "./routes/user/user.js";
 
 const app = new Hono();
+
+app.use(logger());
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
